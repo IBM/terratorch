d53d8cb69bfe896345cdb87d6ccb676a39470dd5
João Lucas de Sousa Almeida <joao.l.sa.9.3@gmail.com>
2024-05-15 14:24:45 -0300

    These files must be removed
    Signed-off-by: João Lucas de Sousa Almeida <joao.l.sa.9.3@gmail.com>

--- build/lib/terratorch/models/backbones/__init__.py
+++
@@ -1,7 +0,0 @@
-# import so they get registered
-from terratorch.models.backbones.prithvi_swin import MMSegSwinTransformer
-from terratorch.models.backbones.prithvi_vit import TemporalViTEncoder
-
-__all__ = ("TemporalViTEncoder", "MMSegSwinTransformer")
-__all__ = ("TemporalViTEncoder", "MMSegSwinTransformer")
-__all__ = ("TemporalViTEncoder", "MMSegSwinTransformer")
--- build/lib/terratorch/models/decoders/__init__.py
+++
@@ -1,6 +0,0 @@
-from terratorch.models.decoders.fcn_decoder import FCNDecoder
-from terratorch.models.decoders.identity_decoder import IdentityDecoder
-from terratorch.models.decoders.upernet_decoder import UperNetDecoder
-from terratorch.models.decoders.satmae_head import SatMAEHead, SatMAEHeadViT
-
-__all__ = ["FCNDecoder", "UperNetDecoder", "IdentityDecoder", "SatMAEHead", "SatMAEHeadViT"]
