6905513692a43aa13e7010887188050b242b5396
Carlos Gomes <carlos.gomes@ibm.com>
2024-05-16 15:42:24 +0200

    change copyright headers

--- src/terratorch/__main__.py
+++ src/terratorch/__main__.py
@@ -1,6 +1,3 @@
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License.
-
 """Command-line interface to TerraTorch."""
 
 from terratorch.cli_tools import build_lightning_cli
--- src/terratorch/models/backbones/prithvi_vit.py
+++ src/terratorch/models/backbones/prithvi_vit.py
@@ -1,13 +1,5 @@
-# Copyright (c) Meta Platforms, Inc. and affiliates.
-# All rights reserved.
-
-# This source code is licensed under the license found in the
-# LICENSE file in the root directory of this source tree.
-# --------------------------------------------------------
-# References:
-# timm: https://github.com/rwightman/pytorch-image-models/tree/master/timm
-# DeiT: https://github.com/facebookresearch/deit
-# --------------------------------------------------------
+# Copyright (c) IBM Corp. All rights reserved.
+# Licensed under the MIT License.
 
 import logging
 from functools import partial
