e2cebea6141ad417c877bedf0557afcb30d987d6
João Lucas de Sousa Almeida <joao.l.sa.9.3@gmail.com>
2024-07-02 08:54:51 -0300

    This test is not necessary
    Signed-off-by: João Lucas de Sousa Almeida <joao.l.sa.9.3@gmail.com>

--- tests/test_backbones.py
+++ tests/test_backbones.py
@@ -50,24 +50,6 @@ def test_vit_models_accept_multitemporal(model_name, input_224_multitemporal):
     backbone = timm.create_model(model_name, pretrained=False, num_frames=NUM_FRAMES)
     backbone(input_224_multitemporal)
 
-@pytest.mark.parametrize("model_name", ["prithvi_swin_B", "prithvi_swin_L", "prithvi_vit_100", "prithvi_vit_300", "prithvi_vit_tiny"])
-def test_all_backbone_instantiation(model_name):
-    
-    if "vit" in model_name :
-        module_str = "terratorch.models.backbones.prithvi_vit"
-    elif "swin" in model_name:
-        module_str = "terratorch.models.backbones.prithvi_swin"
-
-    module_instance = importlib.import_module(module_str)
-
-    model_template = getattr(module_instance, model_name)
-
-    model_instance = model_template()
-
-    torch.save(model_instance.state_dict(), os.path.join("tests/", str(id(model_instance)) + ".pth"))
-
-    model_restored = torch.load(os.path.join("tests/", str(id(model_instance)) + ".pth"))
-
 #def test_swin_models_accept_non_divisible_by_patch_size(input_386):
 #    backbone = timm.create_model("prithvi_swin_90_us", pretrained=False, num_frames=NUM_FRAMES)
 #    backbone(input_386)
