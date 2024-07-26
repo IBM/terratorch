6b870cf5ee6d20bdb31a89728ac787c119b8b058
Carlos Gomes <carlosmiguel.gomes@live.com.pt>
2024-07-26 12:00:37 +0200

    accept mixed band specifications
    Signed-off-by: Carlos Gomes <carlosmiguel.gomes@live.com.pt>

--- tests/test_generic_dataset.py
+++ tests/test_generic_dataset.py
@@ -6,13 +6,64 @@ import pytest
 import torch
 from _pytest.tmpdir import TempPathFactory
 
-from terratorch.datasets import GenericNonGeoPixelwiseRegressionDataset, GenericNonGeoSegmentationDataset
+from terratorch.datasets import GenericNonGeoPixelwiseRegressionDataset, GenericNonGeoSegmentationDataset, HLSBands
 
 REGRESSION_IMAGE_PATH = "tests/regression_test_input.tif"
 REGRESSION_LABEL_PATH = "tests/regression_test_label.tif"
 SEGMENTATION_IMAGE_PATH = "tests/segmentation_test_input.tif"
 SEGMENTATION_LABEL_PATH = "tests/segmentation_test_label.tif"
 NUM_CLASSES_SEGMENTATION = 2
+
+# Testing bands
+# HLS_bands
+HLS_dataset_bands = [
+    "COASTAL_AEROSOL",
+    "BLUE",
+    "GREEN",
+    "RED",
+    "NIR_NARROW",
+    "SWIR_1",
+    "SWIR_2",
+    "CIRRUS",
+    "THEMRAL_INFRARED_1",
+    "THEMRAL_INFRARED_2",
+]
+
+HLS_output_bands = [
+    "BLUE",
+    "GREEN",
+    "RED",
+    "NIR_NARROW",
+    "SWIR_1",
+    "SWIR_2",
+]
+
+HLS_expected_filter_bands = list(range(1, 7))
+# Integer Intervals bands
+int_dataset_bands = [(0, 20)]
+int_output_bands = [(1, 6), (10, 12)]
+# Simple string bands
+str_dataset_bands = [f"band_{j}" for j in range(20)]
+str_output_bands = [f"band_{j}" for j in range(1, 7)] + [f"band_{j}" for j in range(10, 13)]
+
+expected_filter_indices = list(range(1, 7)) + list(range(10, 13))
+
+
+# Mixed case
+mixed_dataset_bands = [
+    (0, 10),
+    HLSBands.RED,
+    HLSBands.BLUE,
+    HLSBands.GREEN,
+    "extra_band_1",
+    "extra_band_2",
+    200,
+    201,
+    202,
+]
+mixed_output_bands = [1, 2, HLSBands.BLUE, "extra_band_1", 201, 202]
+expected_mixed_filter_indices = [1, 2, 12, 14, 17, 18]
+
 
 @pytest.fixture(scope="session")
 def split_file_path(tmp_path_factory):
@@ -58,6 +109,65 @@ class TestGenericRegressionDataset:
         assert torch.is_floating_point(regression_dataset[0]["image"])
         assert torch.is_floating_point(regression_dataset[0]["mask"])
 
+    @pytest.fixture(scope="class")
+    def regression_dataset_with_HLS_bands(self, data_root_regression, split_file_path):
+        return GenericNonGeoPixelwiseRegressionDataset(
+            data_root_regression,
+            dataset_bands=HLS_dataset_bands,
+            output_bands=HLS_output_bands,
+            image_grep="input_data/*_img.tif",
+            label_grep="label_data/*_label.tif",
+            split=split_file_path,
+        ), HLS_expected_filter_bands
+
+    @pytest.fixture(scope="class")
+    def regression_dataset_with_interval_bands(self, data_root_regression, split_file_path):
+        return GenericNonGeoPixelwiseRegressionDataset(
+            data_root_regression,
+            dataset_bands=int_dataset_bands,
+            output_bands=int_output_bands,
+            image_grep="input_data/*_img.tif",
+            label_grep="label_data/*_label.tif",
+            split=split_file_path,
+        ), expected_filter_indices
+
+    @pytest.fixture(scope="class")
+    def regression_dataset_with_str_bands(self, data_root_regression, split_file_path):
+        return GenericNonGeoPixelwiseRegressionDataset(
+            data_root_regression,
+            dataset_bands=str_dataset_bands,
+            output_bands=str_output_bands,
+            image_grep="input_data/*_img.tif",
+            label_grep="label_data/*_label.tif",
+            split=split_file_path,
+        ), expected_filter_indices
+
+    @pytest.fixture(scope="class")
+    def regression_dataset_with_mixed_bands(self, data_root_regression, split_file_path):
+        return GenericNonGeoPixelwiseRegressionDataset(
+            data_root_regression,
+            dataset_bands=mixed_dataset_bands,
+            output_bands=mixed_output_bands,
+            image_grep="input_data/*_img.tif",
+            label_grep="label_data/*_label.tif",
+            split=split_file_path,
+        ), expected_mixed_filter_indices
+
+    @pytest.mark.parametrize(
+        "dataset",
+        [
+            "regression_dataset_with_HLS_bands",
+            "regression_dataset_with_str_bands",
+            "regression_dataset_with_interval_bands",
+            "regression_dataset_with_str_bands",
+            "regression_dataset_with_mixed_bands",
+        ],
+    )
+    def test_correct_filter(self, dataset, request):
+        fixture, expected = request.getfixturevalue(dataset)
+        assert fixture.filter_indices == expected
+
+
 class TestGenericSegmentationDataset:
     @pytest.fixture(scope="class")
     def data_root_segmentation(self, tmp_path_factory: TempPathFactory):
@@ -93,103 +203,64 @@ class TestGenericSegmentationDataset:
         assert torch.is_floating_point(segmentation_dataset[0]["image"])
         assert not torch.is_floating_point(segmentation_dataset[0]["mask"])
 
-# Testing bands
-# HLS_bands
-HLS_dataset_bands = [
-          "COASTAL_AEROSOL",
-          "BLUE",
-          "GREEN",
-          "RED",
-          "NIR_NARROW",
-          "SWIR_1",
-          "SWIR_2",
-          "CIRRUS",
-          "THEMRAL_INFRARED_1",
-          "THEMRAL_INFRARED_2",
-]
-
-HLS_output_bands = [
-          "BLUE",
-          "GREEN",
-          "RED",
-          "NIR_NARROW",
-          "SWIR_1",
-          "SWIR_2",
-] 
-
-# Integer Intervals bands
-int_dataset_bands = (0,10)
-int_output_bands = (1,6)
-# Simple string bands
-str_dataset_bands = [f"band_{j}" for j in range(10)]
-str_output_bands = [f"band_{j}" for j in range(1,6)]
-
-
-class TestGenericDatasetWithBands:
     @pytest.fixture(scope="class")
-    def data_root_regression(self, tmp_path_factory: TempPathFactory):
-        data_dir = tmp_path_factory.mktemp("data")
-        image_dir_path = data_dir / "input_data"
-        label_dir_path = data_dir / "label_data"
-        os.mkdir(image_dir_path)
-        os.mkdir(label_dir_path)
-        for i in range(10):
-            os.symlink(REGRESSION_IMAGE_PATH, image_dir_path / f"{i}_img.tif")
-            os.symlink(REGRESSION_LABEL_PATH, label_dir_path / f"{i}_label.tif")
-
-        # add a few with no suffix
-        for i in range(10, 15):
-            os.symlink(REGRESSION_IMAGE_PATH, image_dir_path / f"{i}.tif")
-            os.symlink(REGRESSION_LABEL_PATH, label_dir_path / f"{i}.tif")
-        return data_dir
-
-    @pytest.fixture(scope="class")
-    def regression_dataset_with_HLS_bands(self, data_root_regression, split_file_path):
-        return GenericNonGeoPixelwiseRegressionDataset(
-            data_root_regression,
+    def segmentation_dataset_with_HLS_bands(self, data_root_segmentation, split_file_path):
+        return GenericNonGeoSegmentationDataset(
+            data_root_segmentation,
+            NUM_CLASSES_SEGMENTATION,
             dataset_bands=HLS_dataset_bands,
             output_bands=HLS_output_bands,
             image_grep="input_data/*_img.tif",
             label_grep="label_data/*_label.tif",
             split=split_file_path,
-        )
+        ), HLS_expected_filter_bands
 
     @pytest.fixture(scope="class")
-    def regression_dataset_with_interval_bands(self, data_root_regression, split_file_path):
-        return GenericNonGeoPixelwiseRegressionDataset(
-            data_root_regression,
-            dataset_bands=[int_dataset_bands],
-            output_bands=[int_output_bands],
+    def segmentation_dataset_with_interval_bands(self, data_root_segmentation, split_file_path):
+        return GenericNonGeoSegmentationDataset(
+            data_root_segmentation,
+            NUM_CLASSES_SEGMENTATION,
+            dataset_bands=int_dataset_bands,
+            output_bands=int_output_bands,
             image_grep="input_data/*_img.tif",
             label_grep="label_data/*_label.tif",
             split=split_file_path,
-        )
+        ), expected_filter_indices
 
     @pytest.fixture(scope="class")
-    def regression_dataset_with_str_bands(self, data_root_regression, split_file_path):
-        return GenericNonGeoPixelwiseRegressionDataset(
-            data_root_regression,
+    def segmentation_dataset_with_str_bands(self, data_root_segmentation, split_file_path):
+        return GenericNonGeoSegmentationDataset(
+            data_root_segmentation,
+            NUM_CLASSES_SEGMENTATION,
             dataset_bands=str_dataset_bands,
             output_bands=str_output_bands,
             image_grep="input_data/*_img.tif",
             label_grep="label_data/*_label.tif",
             split=split_file_path,
-        )
+        ), expected_filter_indices
 
-    def test_usage_of_HLS_bands(self, regression_dataset_with_HLS_bands):
+    @pytest.fixture(scope="class")
+    def segmentation_dataset_with_mixed_bands(self, data_root_segmentation, split_file_path):
+        return GenericNonGeoSegmentationDataset(
+            data_root_segmentation,
+            NUM_CLASSES_SEGMENTATION,
+            dataset_bands=mixed_dataset_bands,
+            output_bands=mixed_output_bands,
+            image_grep="input_data/*_img.tif",
+            label_grep="label_data/*_label.tif",
+            split=split_file_path,
+        ), expected_mixed_filter_indices
 
-        dataset = regression_dataset_with_HLS_bands
-        assert dataset.output_bands == HLS_output_bands
-
-    def test_usage_of_interval_bands(self, regression_dataset_with_interval_bands):
-
-        dataset = regression_dataset_with_interval_bands
-        int_output_bands_ = list(int_output_bands)
-        int_output_bands_[1] += 1
-        assert dataset.output_bands == list(range(*int_output_bands_))
-
-    def test_usage_of_str_bands(self, regression_dataset_with_str_bands):
-
-        dataset = regression_dataset_with_str_bands
-        assert dataset.output_bands == str_output_bands
-
+    @pytest.mark.parametrize(
+        "dataset",
+        [
+            "segmentation_dataset_with_HLS_bands",
+            "segmentation_dataset_with_str_bands",
+            "segmentation_dataset_with_interval_bands",
+            "segmentation_dataset_with_str_bands",
+            "segmentation_dataset_with_mixed_bands",
+        ],
+    )
+    def test_correct_filter(self, dataset, request):
+        fixture, expected = request.getfixturevalue(dataset)
+        assert fixture.filter_indices == expected
