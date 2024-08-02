9d6414d2a60c5c854b6590512b71dc14023969c9
Carlos Gomes <carlosmiguel.gomes@live.com.pt>
2024-08-02 18:19:02 +0200

    save models to tmp path
    Signed-off-by: Carlos Gomes <carlosmiguel.gomes@live.com.pt>

--- tests/test_finetune.py
+++ tests/test_finetune.py
@@ -1,15 +1,17 @@
+import importlib
+import os
+import subprocess
+
 import pytest
 import timm
 import torch
-import importlib
-import terratorch 
-import subprocess
-import os 
 
+import terratorch
 from terratorch.cli_tools import build_lightning_cli
 
+
 @pytest.mark.parametrize("model_name", ["prithvi_swin_B", "prithvi_swin_L", "prithvi_vit_100", "prithvi_vit_300"])
-def test_finetune_multiple_backbones(model_name):
+def test_finetune_multiple_backbones(model_name, tmp_path):
 
     model_instance = timm.create_model(model_name)
     pretrained_bands = [0, 1, 2, 3, 4, 5]
@@ -17,46 +19,46 @@ def test_finetune_multiple_backbones(model_name):
 
     state_dict = model_instance.state_dict()
 
-    torch.save(state_dict, os.path.join("tests/", model_name + ".pt"))
+    torch.save(state_dict, os.path.join(tmp_path, model_name + ".pt"))
 
     # Running the terratorch CLI
     command_list = ["fit", "-c", f"tests/manufactured-finetune_{model_name}.yaml"]
     _ = build_lightning_cli(command_list)
 
 @pytest.mark.parametrize("model_name", ["prithvi_swin_B"])
-def test_finetune_bands_intervals(model_name):
+def test_finetune_bands_intervals(model_name, tmp_path):
 
     model_instance = timm.create_model(model_name)
 
     state_dict = model_instance.state_dict()
 
-    torch.save(state_dict, os.path.join("tests/", model_name + ".pt"))
+    torch.save(state_dict, os.path.join(tmp_path, model_name + ".pt"))
 
     # Running the terratorch CLI
     command_list = ["fit", "-c", f"tests/manufactured-finetune_{model_name}_band_interval.yaml"]
     _ = build_lightning_cli(command_list)
 
 @pytest.mark.parametrize("model_name", ["prithvi_swin_B"])
-def test_finetune_bands_str(model_name):
+def test_finetune_bands_str(model_name, tmp_path):
 
     model_instance = timm.create_model(model_name)
 
     state_dict = model_instance.state_dict()
 
-    torch.save(state_dict, os.path.join("tests/", model_name + ".pt"))
+    torch.save(state_dict, os.path.join(tmp_path, model_name + ".pt"))
 
     # Running the terratorch CLI
     command_list = ["fit", "-c", f"tests/manufactured-finetune_{model_name}_string.yaml"]
     _ = build_lightning_cli(command_list)
     
 @pytest.mark.parametrize("model_name", ["prithvi_swin_B"])
-def test_finetune_bands_str(model_name):
+def test_finetune_bands_str(model_name, tmp_path):
 
     model_instance = timm.create_model(model_name)
 
     state_dict = model_instance.state_dict()
 
-    torch.save(state_dict, os.path.join("tests/", model_name + ".pt"))
+    torch.save(state_dict, os.path.join(tmp_path, model_name + ".pt"))
 
     # Running the terratorch CLI
     command_list = ["fit", "-c", f"tests/manufactured-finetune_{model_name}_metrics_from_file.yaml"]
