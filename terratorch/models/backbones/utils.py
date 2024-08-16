ec8125ee448b471b5897d805e27cde975c96e2f6
João Lucas de Sousa Almeida <joao.l.sa.9.3@gmail.com>
2024-08-16 14:09:48 -0300

    Intervals must include the upper extreme
    Signed-off-by: João Lucas de Sousa Almeida <joao.l.sa.9.3@gmail.com>

--- terratorch/models/backbones/utils.py
+++ terratorch/models/backbones/utils.py
@@ -19,12 +19,14 @@ def _estimate_in_chans(model_bands: list[HLSBands] | list[str] | tuple[int, int]
     is_sublist, requires_special_eval = _are_sublists_of_int(model_bands)
 
     # Bands as intervals limited by integers
+    # The bands numbering follows the Python convention (starts with 0)
+    # and includes the extrema (so the +1 in order to include the last band)
     if requires_special_eval:
 
         if is_sublist:
-            in_chans = sum([i[-1] - i[0] for i in model_bands])
+            in_chans = sum([i[-1] - i[0] + 1 for i in model_bands])
         else:
-            in_chans = model_bands[-1] - model_bands[0]
+            in_chans = model_bands[-1] - model_bands[0] + 1
     else:
         in_chans = len(model_bands) 
 
