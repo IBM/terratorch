fb2798e606b52648ebefac78ee2f118f01e23ba0
Carlos Gomes <carlos.gomes@ibm.com>
2024-09-03 12:47:00 -0400

    skip flakey tests for now

--- tests/test_finetune.py
+++ tests/test_finetune.py
@@ -21,23 +21,27 @@ def setup_and_cleanup(model_name):
     # os.remove(os.path.join("tests", model_name + ".pt"))
     shutil.rmtree(os.path.join("tests", "all_ecos_random"))
 
+@pytest.mark.skip(reason="Flakey on github runner")
 @pytest.mark.parametrize("model_name", ["prithvi_swin_B", "prithvi_vit_100"])
 def test_finetune_multiple_backbones(model_name):
     command_list = ["fit", "-c", f"tests/resources/configs/manufactured-finetune_{model_name}.yaml"]
     _ = build_lightning_cli(command_list)
 
 
+@pytest.mark.skip(reason="Flakey on github runner")
 @pytest.mark.parametrize("model_name", ["prithvi_swin_B"])
 def test_finetune_bands_intervals(model_name):
     command_list = ["fit", "-c", f"tests/resources/configs/manufactured-finetune_{model_name}_band_interval.yaml"]
     _ = build_lightning_cli(command_list)
 
 
+@pytest.mark.skip(reason="Flakey on github runner")
 @pytest.mark.parametrize("model_name", ["prithvi_swin_B"])
 def test_finetune_bands_str(model_name):
     command_list = ["fit", "-c", f"tests/resources/configs/manufactured-finetune_{model_name}_string.yaml"]
     _ = build_lightning_cli(command_list)
 
+@pytest.mark.skip(reason="Flakey on github runner")
 @pytest.mark.parametrize("model_name", ["prithvi_swin_B"])
 def test_finetune_metrics_from_file(model_name):
 
