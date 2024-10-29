0cee971fb4e2f977ae1c17a9382afceda8133039
Paolo <paolo.fraccaro@ibm.com>
2024-10-29 09:52:18 -0400

    add kwargs to scalar forward

--- terratorch/models/scalar_output_model.py
+++ terratorch/models/scalar_output_model.py
@@ -6,7 +6,7 @@ from torch import nn
 
 from terratorch.models.heads import ClassificationHead
 from terratorch.models.model import AuxiliaryHeadWithDecoderWithoutInstantiatedHead, Model, ModelOutput
-
+import pdb
 
 def freeze_module(module: nn.Module):
     for param in module.parameters():
@@ -75,9 +75,8 @@ class ScalarOutputModel(Model, SegmentationModel):
     def check_input_shape(self, x: torch.Tensor) -> bool:  # noqa: ARG002
         return True
 
-    def forward(self, x: torch.Tensor) -> ModelOutput:
+    def forward(self, x: torch.Tensor, **kwargs) -> ModelOutput:
         """Sequentially pass `x` through model`s encoder, decoder and heads"""
-
         self.check_input_shape(x)
         features = self.encoder(x, **kwargs)
 
