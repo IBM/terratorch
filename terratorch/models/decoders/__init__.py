9b31014d83d045c96886c02eb1a85eef4aa60115
João Lucas de Sousa Almeida <joao.l.sa.9.3@gmail.com>
2024-11-04 14:22:46 -0300

    missing import
    Signed-off-by: João Lucas de Sousa Almeida <joao.l.sa.9.3@gmail.com>

--- terratorch/models/decoders/__init__.py
+++ terratorch/models/decoders/__init__.py
@@ -4,5 +4,6 @@ from terratorch.models.decoders.fcn_decoder import FCNDecoder
 from terratorch.models.decoders.identity_decoder import IdentityDecoder
 from terratorch.models.decoders.satmae_head import SatMAEHead, SatMAEHeadViT
 from terratorch.models.decoders.upernet_decoder import UperNetDecoder
+from terratorch.models.decoders.mlp_decoder import MLPDecoder
 
-__all__ = ["FCNDecoder", "UperNetDecoder", "IdentityDecoder", "SatMAEHead", "SatMAEHeadViT"]
+__all__ = ["FCNDecoder", "UperNetDecoder", "IdentityDecoder", "SatMAEHead", "SatMAEHeadViT", "MLPDecoder"]
