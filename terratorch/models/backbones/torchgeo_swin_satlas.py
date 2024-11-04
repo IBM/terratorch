f0a2d117f2a717f1638ad36371aa9b267e6cd814
Paolo <paolo.fraccaro@ibm.com>
2024-11-04 17:21:30 -0500

    fix bug

--- terratorch/models/backbones/torchgeo_swin_satlas.py
+++ terratorch/models/backbones/torchgeo_swin_satlas.py
@@ -15,7 +15,6 @@ import torch
 from terratorch.datasets.utils import OpticalBands, SARBands
 from terratorch.models.backbones.select_patch_embed_weights import select_patch_embed_weights
 
-
 from terratorch.registry import TERRATORCH_BACKBONE_REGISTRY
 
 class SwinEncoderWrapper(nn.Module):
@@ -49,8 +48,8 @@ class SwinEncoderWrapper(nn.Module):
         self.out_channels = [elem for elem in self.out_channels for _ in range(2)]
         self.out_channels = [x for i, x in enumerate(self.out_channels) if (i in self.out_indices) | (i == (len(self.out_channels)-1)) & (-1 in self.out_indices)]
 
-    def forward(self, x: List[torch.Tensor]) -> torch.Tensor:
-        
+    def forward(self, x: List[torch.Tensor], **kwargs) -> torch.Tensor:
+
         outs = []
         for i, layer in enumerate(self.swin_model.features):
             x = layer(x)
