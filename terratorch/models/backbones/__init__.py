1cbe5dde336f9985c478ae9c0a387845ab033463
Benedikt Blumenstiel <benedikt.blumenstiel@ibm.com>
2024-11-05 09:57:26 +0100

    Added MultiMAE
    Signed-off-by: Benedikt Blumenstiel <benedikt.blumenstiel@ibm.com>

--- terratorch/models/backbones/__init__.py
+++ terratorch/models/backbones/__init__.py
@@ -4,5 +4,6 @@
 import terratorch.models.backbones.prithvi_swin
 import terratorch.models.backbones.prithvi_vit
 import terratorch.models.backbones.scalemae
+import terratorch.models.backbones.multimae_register
 
 from terratorch.models.backbones.unet import UNet
