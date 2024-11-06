d5566bcc13bd335449d47acd006dd82ab4dfec9f
Romeo Kienzler <romeo.kienzler1@ibm.com>
2024-11-06 23:56:01 +0100

    add ft functionality

--- terratorch/tasks/wxc_gravity_wave_task.py
+++ terratorch/tasks/wxc_gravity_wave_task.py
@@ -2,10 +2,12 @@
 
 from torchgeo.trainers import BaseTask
 import torch.nn as nn
+import torch
 
 class WxCGravityWaveTask(BaseTask):
-    def __init__(self, model_factory):
+    def __init__(self, model_factory, learning_rate=0.1):
         self.model_factory = model_factory
+        self.learning_rate = learning_rate
         super().__init__()
 
     def configure_optimizers(self):
@@ -13,3 +15,10 @@ class WxCGravityWaveTask(BaseTask):
     
     def configure_models(self):
         self.model = self.model_factory.build_model(backbone='gravitywave', aux_decoders=None)
+
+    def training_step(self, *args, **kwargs):
+        None
+        
+    def train_dataloader(self):
+        None
+        
