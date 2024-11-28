e6886edde01a5e777e28923b920a164e0024e266
Romeo Kienzler <romeo.kienzler1@ibm.com>
2024-11-28 13:21:20 +0100

    optimize config

--- terratorch/tasks/wxc_gravity_wave_task.py
+++ terratorch/tasks/wxc_gravity_wave_task.py
@@ -5,9 +5,10 @@ import torch.nn as nn
 import torch
 
 class WxCGravityWaveTask(BaseTask):
-    def __init__(self, model_factory, mode, learning_rate=0.1):
+    def __init__(self, model_factory, model_args: dict, mode, learning_rate=0.1):
         if mode not in ['train', 'eval']:
             raise ValueError(f'mode {mode} is not supported. (train, eval)')
+        self.model_args = model_args
         self.model_factory = model_factory
         self.learning_rate = learning_rate
         super().__init__()
@@ -16,7 +17,15 @@ class WxCGravityWaveTask(BaseTask):
         return torch.optim.Adam(self.parameters(), lr=self.learning_rate)
     
     def configure_models(self):
-        self.model = self.model_factory.build_model(backbone='prithviwxc', aux_decoders=None)
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        #self.model = self.model_factory.build_model(backbone='prithviwxc', aux_decoders=None, **self.hparams["model_args"])
+        self.model = self.model_factory.build_model(backbone='prithviwxc', aux_decoders=None, **self.model_args)
+        self.model = self.model.to(device)
+        layer_devices = []
+        for name, module in self.model.named_children():
+            device = next(module.parameters(), torch.tensor([])).device
+            layer_devices.append((name, str(device)))
+        print(layer_devices)
 
     def training_step(self, batch, batch_idx):
         output: torch.Tensor = self.model(batch)
