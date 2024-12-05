e2f009cb7dcf83ea22fcebe62609b1ca1deaaca9
João Lucas de Sousa Almeida <joao.l.sa.9.3@gmail.com>
2024-12-05 15:18:15 -0300

    typo
    Signed-off-by: João Lucas de Sousa Almeida <joao.l.sa.9.3@gmail.com>

--- tests/test_finetune.py
+++ tests/test_finetune.py
@@ -25,7 +25,7 @@ def setup_and_cleanup(model_name):
 #@pytest.mark.skip(reason="Flakey on github runner")
 @pytest.mark.parametrize("model_name", ["prithvi_swin_B", "prithvi_vit_100"])
 @pytest.mark.parametrize("case", ["fit", "test", "validate"])
-def test_finetune_multiple_backbones(model_name):
+def test_finetune_multiple_backbones(model_name, case):
     command_list = [case, "-c", f"tests/resources/configs/manufactured-finetune_{model_name}.yaml"]
     _ = build_lightning_cli(command_list)
 
@@ -34,7 +34,7 @@ def test_finetune_multiple_backbones(model_name):
 #@pytest.mark.skip(reason="Flakey on github runner")
 @pytest.mark.parametrize("model_name", ["prithvi_swin_B"])
 @pytest.mark.parametrize("case", ["fit", "test", "validate"])
-def test_finetune_bands_intervals(model_name):
+def test_finetune_bands_intervals(model_name, case):
     command_list = [case, "-c", f"tests/resources/configs/manufactured-finetune_{model_name}_band_interval.yaml"]
     _ = build_lightning_cli(command_list)
 
@@ -43,7 +43,7 @@ def test_finetune_bands_intervals(model_name):
 #@pytest.mark.skip(reason="Flakey on github runner")
 @pytest.mark.parametrize("model_name", ["prithvi_swin_B"])
 @pytest.mark.parametrize("case", ["fit", "test", "validate"])
-def test_finetune_bands_str(model_name):
+def test_finetune_bands_str(model_name, case):
     command_list = [case, "-c", f"tests/resources/configs/manufactured-finetune_{model_name}_string.yaml"]
     _ = build_lightning_cli(command_list)
 
