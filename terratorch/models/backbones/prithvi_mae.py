6eb914a2de74dc27b25e66fd3e4df71f0d5d30a7
Benedikt Blumenstiel <benedikt.blumenstiel@ibm.com>
2024-12-06 20:46:17 +0100

    Simplify warning
    Signed-off-by: Benedikt Blumenstiel <benedikt.blumenstiel@ibm.com>

--- terratorch/models/backbones/prithvi_mae.py
+++ terratorch/models/backbones/prithvi_mae.py
@@ -28,6 +28,7 @@ from einops import rearrange
 from timm.layers import to_2tuple
 from timm.models.vision_transformer import Block
 
+logger = logging.getLogger(__name__)
 
 def get_3d_sincos_pos_embed(embed_dim, grid_size, add_cls_token=False):
     """
@@ -141,7 +142,6 @@ class PatchEmbed(nn.Module):
             norm_layer: nn.Module | None = None,
             flatten: bool = True,
             bias: bool = True,
-            log_warning: bool = False,
     ):
         super().__init__()
         self.input_size = input_size
@@ -152,26 +152,16 @@ class PatchEmbed(nn.Module):
 
         self.proj = nn.Conv3d(in_chans, embed_dim, kernel_size=patch_size, stride=patch_size, bias=bias)
         self.norm = norm_layer(embed_dim) if norm_layer else nn.Identity()
-        self.log_warning = log_warning
-
-        if self.log_warning:
-            self.log_warning_func = self._logging
-        else:
-            self.log_warning_func = self._bypass_logging
-
-    def _bypass_logging(self, *args):
-        pass
-
-    def _logging(self, x, T, H, W):
-
-        if (T / self.patch_size[0] % 1 or H / self.patch_size[1] % 1 or W / self.patch_size[2] % 1):
-            logging.warning(f"Input {x.shape[-3:]} is not divisible by patch size {self.patch_size}."
-                            f"The border will be ignored, add backbone_padding for pixel-wise tasks.")
+        self.log_warning = True
 
     def forward(self, x):
         B, C, T, H, W = x.shape
 
-        self.log_warning_func(x, T, H, W)
+        if (self.log_warning and
+                (T / self.patch_size[0] % 1 or H / self.patch_size[1] % 1 or W / self.patch_size[2] % 1)):
+            logger.warning(f"Input {x.shape[-3:]} is not divisible by patch size {self.patch_size}."
+                           f"The border will be ignored, add backbone_padding for pixel-wise tasks.")
+            self.log_warning = False
 
         x = self.proj(x)
         if self.flatten:
