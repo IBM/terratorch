fd1599f4c81702f16759b42d3b6b1e9cb0f14ba6
João Lucas de Sousa Almeida <joao.l.sa.9.3@gmail.com>
2025-01-06 17:43:33 -0300

    merging
    Signed-off-by: João Lucas de Sousa Almeida <joao.l.sa.9.3@gmail.com>

--- terratorch/models/backbones/select_patch_embed_weights.py
+++ terratorch/models/backbones/select_patch_embed_weights.py
@@ -10,20 +10,20 @@ from torch import nn
 from terratorch.datasets import HLSBands, OpticalBands, SARBands
 import collections
 
-    def patch_embed_weights_are_compatible(model_patch_embed: torch.Tensor, checkpoint_patch_embed: torch.Tensor) -> bool:
-        # check all dimensions are the same except for channel dimension
-        if len(model_patch_embed.shape) != len(checkpoint_patch_embed.shape):
-                return False
+def patch_embed_weights_are_compatible(model_patch_embed: torch.Tensor, checkpoint_patch_embed: torch.Tensor) -> bool:
+    # check all dimensions are the same except for channel dimension
+    if len(model_patch_embed.shape) != len(checkpoint_patch_embed.shape):
+        return False
 
-        model_shape = [model_patch_embed.shape[i] for i in range(len(model_patch_embed.shape)) if i != 1]
-        checkpoint_shape = [checkpoint_patch_embed.shape[i] for i in range(len(checkpoint_patch_embed.shape)) if i != 1]
-        return model_shape == checkpoint_shape
+    model_shape = [model_patch_embed.shape[i] for i in range(len(model_patch_embed.shape)) if i != 1]
+    checkpoint_shape = [checkpoint_patch_embed.shape[i] for i in range(len(checkpoint_patch_embed.shape)) if i != 1]
+    return model_shape == checkpoint_shape
 
-    def select_patch_embed_weights(
-        state_dict: dict, model: nn.Module, pretrained_bands: list[HLSBands | int | OpticalBands| SARBands], model_bands: list[HLSBands | int | OpticalBands| SARBands], custom_proj_key: str = None
-    ) -> dict:
-        """Filter out the patch embedding weights according to the bands being used.
-        If a band exists in the pretrained_bands, but not in model_bands, drop it.
+def select_patch_embed_weights(
+    state_dict: dict, model: nn.Module, pretrained_bands: list[HLSBands | int | OpticalBands| SARBands], model_bands: list[HLSBands | int | OpticalBands| SARBands], custom_proj_key: str = None
+) -> dict:
+    """Filter out the patch embedding weights according to the bands being used.
+    If a band exists in the pretrained_bands, but not in model_bands, drop it.
         If a band exists in model_bands, but not pretrained_bands, randomly initialize those weights.
 
 
@@ -35,18 +35,18 @@ import collections
 
         Returns:
             dict: New state dict
-        """
-        if (type(pretrained_bands) == type(model_bands)) | (type(pretrained_bands) == int) | (type(model_bands) == int): 
+    """
+    if (type(pretrained_bands) == type(model_bands)) | (type(pretrained_bands) == int) | (type(model_bands) == int): 
 
-            if custom_proj_key is None:
-                _possible_keys_for_proj_weight = {
-                    "patch_embed.proj.weight",
-                    "module.patch_embed.proj.weight",
-                    "patch_embed.projection.weight",
-                    "module.patch_embed.projection.weight",
-                }
-            else:
-                _possible_keys_for_proj_weight = {custom_proj_key}
+        if custom_proj_key is None:
+            _possible_keys_for_proj_weight = {
+                "patch_embed.proj.weight",
+                "module.patch_embed.proj.weight",
+                "patch_embed.projection.weight",
+                "module.patch_embed.projection.weight",
+            }
+        else:
+            _possible_keys_for_proj_weight = {custom_proj_key}
 
             patch_embed_proj_weight_key = state_dict.keys() & _possible_keys_for_proj_weight if (type(state_dict) in [collections.OrderedDict, dict]) else state_dict().keys() & _possible_keys_for_proj_weight
             if len(patch_embed_proj_weight_key) == 0:
@@ -55,13 +55,13 @@ import collections
             if len(patch_embed_proj_weight_key) > 1:
                 msg = "Too many matches for key for patch embed weight"
                 raise Exception(msg)
-        
+
             # extract the single element from the set
             (patch_embed_proj_weight_key,) = patch_embed_proj_weight_key
             patch_embed_weight = state_dict[patch_embed_proj_weight_key]
-        
+
             temp_weight = model.state_dict()[patch_embed_proj_weight_key].clone() 
-        
+
             # only do this if the patch size and tubelet size match. If not, start with random weights
             if patch_embed_weights_are_compatible(temp_weight, patch_embed_weight):
                 torch.nn.init.xavier_uniform_(temp_weight.view([temp_weight.shape[0], -1]))
@@ -76,9 +76,9 @@ import collections
                     category=UserWarning,
                     stacklevel=1,
                 )
-        
+
             state_dict[patch_embed_proj_weight_key] = temp_weight
-            
+
         # extract the single element from the set
         (patch_embed_proj_weight_key,) = patch_embed_proj_weight_key
         patch_embed_weight = state_dict[patch_embed_proj_weight_key]
