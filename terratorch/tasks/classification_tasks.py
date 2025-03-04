3faf8ba16ec49e7a5439ff5bce4be06f41eaa7ee
Benedikt Blumenstiel <benedikt.blumenstiel@ibm.com>
2025-03-04 17:28:51 +0100

    Add fix to classification task
    Signed-off-by: Benedikt Blumenstiel <benedikt.blumenstiel@ibm.com>

--- terratorch/tasks/classification_tasks.py
+++ terratorch/tasks/classification_tasks.py
@@ -217,7 +217,7 @@ class ClassificationTask(TerraTorchTask):
             dataloader_idx: Index of the current dataloader.
         """
         x = batch["image"]
-        y = batch["label"].to(torch.float32)
+        y = batch["label"]
         other_keys = batch.keys() - {"image", "label", "filename"}
         rest = {k: batch[k] for k in other_keys}
 
@@ -238,7 +238,7 @@ class ClassificationTask(TerraTorchTask):
             dataloader_idx: Index of the current dataloader.
         """
         x = batch["image"]
-        y = batch["label"].to(torch.float32)
+        y = batch["label"]
         other_keys = batch.keys() - {"image", "label", "filename"}
         rest = {k: batch[k] for k in other_keys}
         model_output: ModelOutput = self(x, **rest)
@@ -256,7 +256,7 @@ class ClassificationTask(TerraTorchTask):
             dataloader_idx: Index of the current dataloader.
         """
         x = batch["image"]
-        y = batch["label"].to(torch.float32)
+        y = batch["label"]
         other_keys = batch.keys() - {"image", "label", "filename"}
         rest = {k: batch[k] for k in other_keys}
         model_output: ModelOutput = self(x, **rest)
