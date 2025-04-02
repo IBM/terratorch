1137a8b3da2fecc96c8c44f327eec09e6ba528b4
João Lucas de Sousa Almeida <joao.l.sa.9.3@gmail.com>
2025-04-02 14:12:21 -0300

    Simplifying tests/test_prithvi_tasks.py
    Signed-off-by: João Lucas de Sousa Almeida <joao.l.sa.9.3@gmail.com>

--- tests/test_prithvi_tasks.py
+++ tests/test_prithvi_tasks.py
@@ -30,9 +30,35 @@ def model_input() -> torch.Tensor:
 
 @pytest.mark.parametrize("backbone", ["prithvi_eo_v1_100", "prithvi_eo_v2_300", "prithvi_swin_B"])
 @pytest.mark.parametrize("decoder", ["FCNDecoder", "UperNetDecoder", "IdentityDecoder", "UNetDecoder"])
-@pytest.mark.parametrize("loss", ["ce", "jaccard", "focal", "dice"])
+@pytest.mark.parametrize("loss", ["ce"])
 @pytest.mark.parametrize("lr_overrides", [{"encoder": 0.01}, None])
-def test_create_segmentation_task(backbone, decoder, loss, model_factory: str, lr_overrides):
+def test_create_segmentation_task_encoder_decoder(backbone, decoder, loss, model_factory: str, lr_overrides):
+    model_args = {
+        "backbone": backbone,
+        "decoder": decoder,
+        "backbone_bands": PRETRAINED_BANDS,
+        "backbone_pretrained": False,
+        "num_classes": NUM_CLASSES,
+    }
+
+    if decoder in ["UperNetDecoder", "UNetDecoder"] and backbone.startswith("prithvi_eo"):
+        model_args["necks"] = VIT_UPERNET_NECK
+    if decoder == "UNetDecoder":
+        model_args["decoder_channels"] = [256, 128, 64, 32]
+    SemanticSegmentationTask(
+        model_args,
+        model_factory,
+        loss=loss,
+        lr_overrides=lr_overrides,
+    )
+
+    gc.collect()
+
+@pytest.mark.parametrize("backbone", ["prithvi_eo_v2_300"])
+@pytest.mark.parametrize("decoder", ["FCNDecoder", "UperNetDecoder", "IdentityDecoder", "UNetDecoder"])
+@pytest.mark.parametrize("loss", ["ce", "jaccard", "focal", "dice"])
+@pytest.mark.parametrize("lr_overrides", [None])
+def test_create_segmentation_task_decoder_to_optim(backbone, decoder, loss, model_factory: str, lr_overrides):
     model_args = {
         "backbone": backbone,
         "decoder": decoder,
@@ -57,9 +83,35 @@ def test_create_segmentation_task(backbone, decoder, loss, model_factory: str, l
 
 @pytest.mark.parametrize("backbone", ["prithvi_eo_v1_100", "prithvi_eo_v2_300", "prithvi_swin_B"])
 @pytest.mark.parametrize("decoder", ["FCNDecoder", "UperNetDecoder", "IdentityDecoder", "UNetDecoder"])
-@pytest.mark.parametrize("loss", ["mae", "rmse", "huber"])
+@pytest.mark.parametrize("loss", ["mae"])
 @pytest.mark.parametrize("lr_overrides", [{"encoder": 0.01}, None])
-def test_create_regression_task(backbone, decoder, loss, model_factory: str, lr_overrides):
+def test_create_regression_task_encoder_decoder(backbone, decoder, loss, model_factory: str, lr_overrides):
+    model_args = {
+        "backbone": backbone,
+        "decoder": decoder,
+        "backbone_bands": PRETRAINED_BANDS,
+        "backbone_pretrained": False,
+    }
+
+    if decoder in ["UperNetDecoder", "UNetDecoder"] and backbone.startswith("prithvi_eo"):
+        model_args["necks"] = VIT_UPERNET_NECK
+    if decoder == "UNetDecoder":
+        model_args["decoder_channels"] = [256, 128, 64, 32]
+
+    PixelwiseRegressionTask(
+        model_args,
+        model_factory,
+        loss=loss,
+        lr_overrides=lr_overrides,
+    )
+
+    gc.collect()
+
+@pytest.mark.parametrize("backbone", ["prithvi_eo_v2_300"])
+@pytest.mark.parametrize("decoder", ["FCNDecoder", "UperNetDecoder", "IdentityDecoder", "UNetDecoder"])
+@pytest.mark.parametrize("loss", ["mae", "rmse", "huber"])
+@pytest.mark.parametrize("lr_overrides", [None])
+def test_create_regression_task_decoder_to_optim(backbone, decoder, loss, model_factory: str, lr_overrides):
     model_args = {
         "backbone": backbone,
         "decoder": decoder,
@@ -84,9 +136,9 @@ def test_create_regression_task(backbone, decoder, loss, model_factory: str, lr_
 
 @pytest.mark.parametrize("backbone", ["prithvi_eo_v1_100", "prithvi_eo_v2_300", "prithvi_swin_B"])
 @pytest.mark.parametrize("decoder", ["FCNDecoder", "UperNetDecoder", "IdentityDecoder", "UNetDecoder"])
-@pytest.mark.parametrize("loss", ["ce", "bce", "jaccard", "focal"])
+@pytest.mark.parametrize("loss", ["ce"])
 @pytest.mark.parametrize("lr_overrides", [{"encoder": 0.01}, None])
-def test_create_classification_task(backbone, decoder, loss, model_factory: str, lr_overrides):
+def test_create_classification_task_encoder_decoder(backbone, decoder, loss, model_factory: str, lr_overrides):
     model_args = {
         "backbone": backbone,
         "decoder": decoder,
@@ -108,3 +160,32 @@ def test_create_classification_task(backbone, decoder, loss, model_factory: str,
     )
 
     gc.collect()
+
+@pytest.mark.parametrize("backbone", ["prithvi_eo_v2_300"])
+@pytest.mark.parametrize("decoder", ["FCNDecoder", "UperNetDecoder", "IdentityDecoder", "UNetDecoder"])
+@pytest.mark.parametrize("loss", ["ce", "bce", "jaccard", "focal"])
+@pytest.mark.parametrize("lr_overrides", [None])
+def test_create_classification_task_decoder_to_optim(backbone, decoder, loss, model_factory: str, lr_overrides):
+    model_args = {
+        "backbone": backbone,
+        "decoder": decoder,
+        "backbone_bands": PRETRAINED_BANDS,
+        "backbone_pretrained": False,
+        "num_classes": NUM_CLASSES,
+    }
+
+    if decoder in ["UperNetDecoder", "UNetDecoder"] and backbone.startswith("prithvi_eo"):
+        model_args["necks"] = VIT_UPERNET_NECK
+    if decoder == "UNetDecoder":
+        model_args["decoder_channels"] = [256, 128, 64, 32]
+
+    ClassificationTask(
+        model_args,
+        model_factory,
+        loss=loss,
+        lr_overrides=lr_overrides,
+    )
+
+    gc.collect()
+
+
