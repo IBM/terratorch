99757b07c05be957b084a326928dba33a690606b
Romeo Kienzler <romeo.kienzler1@ibm.com>
2025-04-14 12:12:50 +0200

    start terratorch init work by predownloading model configs in docker build
    Signed-off-by: Romeo Kienzler <romeo.kienzler1@ibm.com>

--- terratorch/__main__.py
+++ terratorch/__main__.py
@@ -9,6 +9,8 @@ from jsonargparse import ArgumentParser
 from terratorch.cli_tools import build_lightning_cli
 import sys
 from typing import List, Any
+from huggingface_hub import hf_hub_download
+
 try:
     from benchmark.benchmark_types import Defaults, Task
     from benchmark.backbone_benchmark import benchmark_backbone, rerun_best_from_backbone
@@ -138,6 +140,77 @@ def main():
                 n_trials=n_trials,
                 run_repetitions=run_repetitions,
             )
+    elif sys.argv[1] == "init":
+        logger = logging.getLogger("terratorch-init")
+        logger.info("Initializing TerraTorch...")
+        logger.info("Downloading model congfigs...")
+        files_to_download = [
+            {
+                'repo_id': 'ibm-granite/granite-geospatial-biomass',
+            },
+            {
+                'repo_id': 'ibm-granite/granite-geospatial-wxc-downscaling',
+            },
+            {
+                'repo_id': 'ibm-granite/granite-geospatial-canopyheight',
+            },
+            {
+                'repo_id': 'ibm-granite/granite-geospatial-land-surface-temperature',
+            },
+            {
+                'repo_id': 'ibm-granite/granite-geospatial-uki',
+            },
+            {
+                'repo_id': 'ibm-granite/granite-geospatial-uki-flooddetection',
+            },
+            {
+                'repo_id': 'ibm-nasa-geospatial/Prithvi-WxC-1.0-2300M',
+            },
+            {
+                'repo_id': 'ibm-nasa-geospatial/Prithvi-WxC-1.0-2300M-rollout',
+            },
+            {
+                'repo_id': 'ibm-nasa-geospatial/Prithvi-WxC-1.0-2300m-gravity-wave-parameterization',
+            },
+            {
+                'repo_id': 'ibm-nasa-geospatial/Prithvi-EO-2.0-300M-TL',
+                'filename': 'config.json',
+            },
+            {
+                'repo_id': 'ibm-nasa-geospatial/Prithvi-EO-2.0-600M-TL',
+                'filename': 'config.json',
+            },
+            {
+                'repo_id': 'ibm-nasa-geospatial/Prithvi-EO-2.0-300M-BurnScars',
+                'filename': 'burn_scars_config.yaml',
+            },
+            {
+                'repo_id': 'ibm-nasa-geospatial/Prithvi-EO-1.0-100M-burn-scar',
+            },
+            {
+                'repo_id': 'ibm-nasa-geospatial/Prithvi-EO-1.0-100M-multi-temporal-crop-classification',
+            },
+            {
+                'repo_id': 'ibm-nasa-geospatial/Prithvi-EO-2.0-300M',
+                'filename': 'config.json',
+            },
+            {
+                'repo_id': 'ibm-nasa-geospatial/Prithvi-EO-1.0-100M',
+            },
+            {
+                'repo_id': 'ibm-nasa-geospatial/Prithvi-EO-2.0-300M-TL-Sen1Floods11',
+            },
+            {
+                'repo_id': 'ibm-nasa-geospatial/Prithvi-EO-1.0-100M-sen1floods11',
+            },
+        ]
+
+        for config in files_to_download:
+            file_path = hf_hub_download(
+                repo_id=config['repo_id'],
+                filename=config.get('filename', '') or 'config.yaml',
+            )
+            logger.info(f"File downloaded to: {file_path}")
     else:
         _ = build_lightning_cli()
 
