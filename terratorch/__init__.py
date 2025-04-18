a4ca8df7c7f22ddf469f372e1099157d2d7beeb2
Benedikt Blumenstiel <benedikt.blumenstiel@ibm.com>
2025-04-18 19:04:23 +0200

    Add FULL_MODEL_REGISTRY import
    Signed-off-by: Benedikt Blumenstiel <benedikt.blumenstiel@ibm.com>

--- terratorch/__init__.py
+++ terratorch/__init__.py
@@ -2,4 +2,4 @@
 
 import terratorch.models  # noqa: F401
 from terratorch.models.backbones import *  # register models in registries # noqa: F403
-from terratorch.registry import BACKBONE_REGISTRY, DECODER_REGISTRY, MODEL_FACTORY_REGISTRY  # noqa: F401
+from terratorch.registry import BACKBONE_REGISTRY, DECODER_REGISTRY, MODEL_FACTORY_REGISTRY, FULL_MODEL_REGISTRY  # noqa: F401
