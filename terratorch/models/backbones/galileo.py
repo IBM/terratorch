5384878994f9ecd22162cbbb7ac326f7bd3b53f1
João Lucas de Sousa Almeida <joao.l.sa.9.3@gmail.com>
2025-06-20 10:41:05 -0300

    try-except instructing how to install galileo
    Signed-off-by: João Lucas de Sousa Almeida <joao.l.sa.9.3@gmail.com>

--- terratorch/models/backbones/galileo.py
+++ terratorch/models/backbones/galileo.py
@@ -1,26 +1,30 @@
 import logging
+
+import huggingface_hub
 import torch
 from torch import nn
-import huggingface_hub
+
 from terratorch.registry import TERRATORCH_BACKBONE_REGISTRY
 
-from galileo.galileo import GalileoWrapper
+try:
+    from galileo.galileo import GalileoWrapper
+except:
+    logging.getLogger("terratorch").debug(
+        "The package `galileo` is not installed. If you want to use it, install it using"
+        "`pip install git+https://github.com/Joao-L-S-Almeida/terratorch-galileo.git`"
+    )
+
 
 def load_weights(model: nn.Module, ckpt_data: dict, **kwargs) -> nn.Module:
-
-    print(f"Loading weights")
+    print("Loading weights")
     if ckpt_data is not None:
-
         ckpt_data = huggingface_hub.hf_hub_download(**ckpt_data)
 
         checkpoint_model = torch.load(ckpt_data, map_location="cpu", weights_only=True)
         state_dict = model.state_dict()
 
         for k in ["head.weight", "head.bias"]:
-            if (
-                k in checkpoint_model
-                and checkpoint_model[k].shape != state_dict[k].shape
-            ):
+            if k in checkpoint_model and checkpoint_model[k].shape != state_dict[k].shape:
                 logging.info(f"Removing key {k} from pretrained checkpoint")
                 del checkpoint_model[k]
 
@@ -29,64 +33,47 @@ def load_weights(model: nn.Module, ckpt_data: dict, **kwargs) -> nn.Module:
 
     return model
 
-@TERRATORCH_BACKBONE_REGISTRY.register
-def galileo_tiny_encoder(pretrained:bool=None, ckpt_data: str | None = None, **kwargs):
 
-    remote_checkpoint_path = {
-        "repo_id":
-        "nasaharvest/galileo",
-        "subfolder": "models/tiny",
-        "filename": "encoder.pt"
-    }
+@TERRATORCH_BACKBONE_REGISTRY.register
+def galileo_tiny_encoder(pretrained: bool = None, ckpt_data: str | None = None, **kwargs):
+    remote_checkpoint_path = {"repo_id": "nasaharvest/galileo", "subfolder": "models/tiny", "filename": "encoder.pt"}
 
     if not ckpt_data:
-      ckpt_data = remote_checkpoint_path
+        ckpt_data = remote_checkpoint_path
 
     model = GalileoWrapper(**kwargs)
 
     if pretrained:
-      model = load_weights(model, ckpt_data)
+        model = load_weights(model, ckpt_data)
 
-    return model 
+    return model
+
 
 @TERRATORCH_BACKBONE_REGISTRY.register
-def galileo_base_encoder(pretrained:bool=None, ckpt_data: str | None = None, **kwargs):
-
-    remote_checkpoint_path = {
-        "repo_id":
-        "nasaharvest/galileo",
-        "subfolder": "models/base",
-        "filename": "encoder.pt"
-    }
+def galileo_base_encoder(pretrained: bool = None, ckpt_data: str | None = None, **kwargs):
+    remote_checkpoint_path = {"repo_id": "nasaharvest/galileo", "subfolder": "models/base", "filename": "encoder.pt"}
 
     if not ckpt_data:
-      ckpt_data = remote_checkpoint_path
+        ckpt_data = remote_checkpoint_path
 
     model = GalileoWrapper(**kwargs)
 
     if pretrained:
-      model = load_weights(model, ckpt_data)
+        model = load_weights(model, ckpt_data)
 
-    return model 
+    return model
+
 
 @TERRATORCH_BACKBONE_REGISTRY.register
-def galileo_nano_encoder(pretrained:bool=None, ckpt_data: str | None = None, **kwargs):
-
-    remote_checkpoint_path = {
-        "repo_id":
-        "nasaharvest/galileo",
-        "subfolder": "models/nano",
-        "filename": "encoder.pt"
-    }
+def galileo_nano_encoder(pretrained: bool = None, ckpt_data: str | None = None, **kwargs):
+    remote_checkpoint_path = {"repo_id": "nasaharvest/galileo", "subfolder": "models/nano", "filename": "encoder.pt"}
 
     if not ckpt_data:
-      ckpt_data = remote_checkpoint_path
+        ckpt_data = remote_checkpoint_path
 
     model = GalileoWrapper(**kwargs)
 
     if pretrained:
-      model = load_weights(model, ckpt_data)
+        model = load_weights(model, ckpt_data)
 
-    return model 
-
-
+    return model
