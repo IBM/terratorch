26ca9a8d6031e184ebb7e6233a32b6f1e0449714
João Lucas de Sousa Almeida <joao.l.sa.9.3@gmail.com>
2025-06-20 15:20:59 -0300

    GalileoDecoder is no more necessary
    Signed-off-by: João Lucas de Sousa Almeida <joao.l.sa.9.3@gmail.com>

--- terratorch/models/decoders/__init__.py
+++ terratorch/models/decoders/__init__.py
@@ -1,26 +1,24 @@
 # Copyright contributors to the Terratorch project
 
+from terratorch.models.decoders.aspp_head import ASPPRegressionHead, ASPPSegmentationHead
 from terratorch.models.decoders.fcn_decoder import FCNDecoder
 from terratorch.models.decoders.identity_decoder import IdentityDecoder
-from terratorch.models.decoders.satmae_head import SatMAEHead, SatMAEHeadViT
-from terratorch.models.decoders.upernet_decoder import UperNetDecoder
-from terratorch.models.decoders.aspp_head import ASPPSegmentationHead, ASPPRegressionHead
-from terratorch.models.decoders.mlp_decoder import MLPDecoder
-from terratorch.models.decoders.unet_decoder import UNetDecoder
 from terratorch.models.decoders.linear_decoder import LinearDecoder
-from terratorch.models.decoders.galileo_decoder import GalileoDecoder
+from terratorch.models.decoders.mlp_decoder import MLPDecoder
+from terratorch.models.decoders.satmae_head import SatMAEHead, SatMAEHeadViT
+from terratorch.models.decoders.unet_decoder import UNetDecoder
+from terratorch.models.decoders.upernet_decoder import UperNetDecoder
 
 __all__ = [
+    "ASPPRegressionHead",
+    "ASPPSegmentationHead",
     "FCNDecoder",
-    "UperNetDecoder",
     "IdentityDecoder",
+    "LinearDecoder",
+    "MLPDecoder",
+    "SMPDecoderWrapper",
     "SatMAEHead",
     "SatMAEHeadViT",
-    "SMPDecoderWrapper",
-    "ASPPSegmentationHead",
-    "ASPPRegressionHead",
-    "MLPDecoder",
     "UNetDecoder",
-    "LinearDecoder",
-    "GalileoDecoder",
+    "UperNetDecoder",
 ]
