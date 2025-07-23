bbaebe325546623e28a826c87726edd1eaa596c4
João Lucas de Sousa Almeida <joao.l.sa.9.3@gmail.com>
2025-07-23 09:56:34 -0300

    Updating integration tests
    Signed-off-by: João Lucas de Sousa Almeida <joao.l.sa.9.3@gmail.com>

--- integrationtests/run_base_set.sh
+++ integrationtests/run_base_set.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-jbsub -q x86_1h -mem 24G -cores 1x2+1 -interactive 'cd /dccstor/terratorch/users/rkie/gitco/terratorch && source .venv/bin/activate && cd integrationtests && time pytest test_base_set.py'
-/opt/share/exec/jbsub8 -q x86_1h -mem 24G -cores 1x2+1 -interactive cd /dccstor/terratorch/users/rkie/gitco/terratorch && source .venv/bin/activate && cd integrationtests && time pytest test_base_set.py
+
+bsub -q interactive -M 24G -c 2 -R "rusage[mem=8G]" -gpu "num=1:mode=shared:j_exclusive=yes" 'cd /dccstor/terratorch/users/rkie/gitco/terratorch && source .venv/bin/activate && cd integrationtests && time pytest test_base_set.py'
+
+# For jbsub 
+#jbsub -q x86_1h -mem 24G -cores 1x2+1 -interactive 'cd /dccstor/terratorch/users/rkie/gitco/terratorch && source .venv/bin/activate && cd integrationtests && time pytest test_base_set.py'
+
+#/opt/share/exec/jbsub8 -q x86_1h -mem 24G -cores 1x2+1 -interactive cd /dccstor/terratorch/users/rkie/gitco/terratorch && source .venv/bin/activate && cd integrationtests && time pytest test_base_set.py
