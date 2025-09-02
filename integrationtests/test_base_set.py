45d7d28320ab02d7ce0469e783e98ec9a57d82cf
Catherine Wanjiru <catherine.wanjiru@ibm.com>
2025-09-02 09:31:52 -0400

    Added Surya tests
    Signed-off-by: Catherine Wanjiru <catherine.wanjiru@ibm.com>

--- integrationtests/test_base_set.py
+++ integrationtests/test_base_set.py
@@ -7,7 +7,12 @@ import shutil
 import re
 import subprocess
 import gc
+import pandas as pd
+from datetime import datetime
+import time
 
+from huggingface_hub import hf_hub_download
+from terratorch.datasets import HelioNetCDFDataset
 from terratorch.cli_tools import LightningInferenceModel
 
 
@@ -168,6 +173,74 @@ def test_burnscars_predict(burnscars_image, model_name):
 
     gc.collect()
 
+
+def test_surya():
+    """
+    Additional test for the Surya model
+    """
+    root_dir = "/dccstor/terratorch/users/wanjiru/surya-data/experiment"
+
+    hf_hub_download(repo_id="nasa-ibm-ai4science/Surya-1.0", filename="scalers.yaml", local_dir=root_dir)
+
+    # Creating index file
+    sample_files = glob.glob(os.path.join(root_dir, "*.nc"))
+    paths = sorted(sample_files)
+    present = len(paths) * [1]
+
+    timestamps = []
+    for ff in paths:
+        filename = os.path.basename(ff).replace(".nc", "")
+        date, timestamp = filename.split("_")
+
+        year = int(date[:4])
+        month = int(date[4:6])
+        day = int(date[6:])
+        hour = int(timestamp[:2])
+        minutes = int(timestamp[2:])
+        seconds = 0
+
+        date_datetime = datetime(year, month, day, hour, minutes, seconds).strftime("%Y-%m-%d %H:%M:%S")
+
+        timestamps.append(date_datetime)
+
+    index_dict = {"path": paths, "timestep": timestamps, "present": present}
+    index_dataframe = pd.DataFrame(index_dict)
+    index_dataframe.to_csv(os.path.join(root_dir, "index.csv"))
+    index_path = os.path.join(root_dir, "index.csv")
+    scalers_path = os.path.join(root_dir, "scalers.yaml")
+
+    channels = [
+        "aia94",
+        "aia131",
+        "aia171",
+        "aia193",
+        "aia211",
+        "aia304",
+        "aia335",
+        "aia1600",
+        "hmi_m",
+        "hmi_bx",
+        "hmi_by",
+        "hmi_bz",
+        "hmi_v",
+    ]
+
+    dataset = HelioNetCDFDataset(
+        index_path=index_path,
+        time_delta_input_minutes=[-60, 0],
+        time_delta_target_minutes=+60,
+        channels=channels,
+        n_input_timestamps=2,
+        rollout_steps=1,
+        scalers=scalers_path,
+    )
+
+    start_time = time.time()
+    for batch in dataset:
+        batch_ = {k: torch.from_numpy(v).to("cuda:0") for k, v in batch[0].items()}
+    print(f"Elapsed time: {time.time() - start_time} s")
+
+    assert isinstance(batch_, dict) and len(batch_)>0
 
 ## Only run these tests after running test_finetune.py.
 ## Uses the recently created checkpoints to test if the
