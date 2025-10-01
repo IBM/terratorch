1b6d2630acaa77a60467b77d066217fa76639d0d
Christian Pinto <christian.pinto@ibm.com>
2025-10-01 14:21:53 +0000

    improved script to execute vLLM tests
    Signed-off-by: Christian Pinto <christian.pinto@ibm.com>

--- integrationtests/vLLM/test_segmentation_io_processor.py
+++ integrationtests/vLLM/test_segmentation_io_processor.py
@@ -16,13 +16,13 @@ from .config import models, inputs
 models_output = {
     "prithvi_300m_sen1floods11": {
             "india_url_in_base64_out": "f7dc282de2c36942",
-            # "valencia_url_in_base64_out": "aa6d92ad25926a5e",
-            # "valencia_url_in_path_out": "aa6d92ad25926a5e",
+            "valencia_url_in_base64_out": "aa6d92ad25926a5e",
+            "valencia_url_in_path_out": "aa6d92ad25926a5e",
         },
-    # "prithvi_300m_burnscars": {
-    #     "burnscars_url_in_base64_out": "c17c4f602ea7b616",
-    #     "burnscars_url_in_path_out": "c17c4f602ea7b616"
-    # }
+    "prithvi_300m_burnscars": {
+        "burnscars_url_in_base64_out": "c17c4f602ea7b616",
+        "burnscars_url_in_path_out": "c17c4f602ea7b616"
+    }
 }
 
 tests_per_model = [(model, input) for model in models_output.keys() for input in models_output[model].keys() ]
--- integrationtests/vllm_tests_set.sh
+++ integrationtests/vllm_tests_set.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-WORKING_DIR=$1
+SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
+WORKING_DIR=${SCRIPT_DIR}/../../
+pushd ${WORKING_DIR}
 
 VENV_NAME=vllm_integration_tests_venv
 
-cd ${WORKING_DIR}
-pushd ${WORKING_DIR}
-
-uv venv --python 3.12 ${VENV_NAME}
+uv venv --clear --python 3.12 ${VENV_NAME}
 source ${VENV_NAME}/bin/activate
-
 cd terratorch
 
 uv pip install -e .[test]
