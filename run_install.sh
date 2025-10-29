06fdcab66f0b653cc0c42632f3153f25786cf568
João Lucas de Sousa Almeida <joao.l.sa.9.3@gmail.com>
2025-10-29 10:28:21 -0300

    Updating containerization scripts
    Signed-off-by: João Lucas de Sousa Almeida <joao.l.sa.9.3@gmail.com>

--- run_install.sh
+++ run_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-uv pip install --system git+https://github.com/The-AI-Alliance/GEO-Bench-2.git@main
-uv pip install --system -e .[wxc,test]
+pip install --user git+https://github.com/The-AI-Alliance/GEO-Bench-2.git@main
+pip install --user -e .[wxc,test]
 
 
