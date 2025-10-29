95d5f9fccb8b238373f03f1575af77ea53b65fa4
João Lucas de Sousa Almeida <joao.l.sa.9.3@gmail.com>
2025-10-29 17:40:59 -0300

    Updating scripts
    Signed-off-by: João Lucas de Sousa Almeida <joao.l.sa.9.3@gmail.com>

--- run_install.sh
+++ run_install.sh
@@ -3,4 +3,6 @@ pip install --upgrade pip
 pip install --user git+https://github.com/The-AI-Alliance/GEO-Bench-2.git@main
 pip install --user -e .[wxc,test]
 
+pytest -s --cov=terratorch -v --cov-report term-missing tests
+
 
