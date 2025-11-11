1b6ee5f4330d9170e96cf1d2ed645b5ceea7d6f8
João Lucas de Sousa Almeida <joao.l.sa.9.3@gmail.com>
2025-11-11 16:57:54 -0300

    Updatinf scripts
    Signed-off-by: João Lucas de Sousa Almeida <joao.l.sa.9.3@gmail.com>
    
    It isn't necessary to install it two times
    
    Signed-off-by: João Lucas de Sousa Almeida <joao.l.sa.9.3@gmail.com>

--- run_install.sh
+++ run_install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 pip install --upgrade pip
-pip install --user git+https://github.com/The-AI-Alliance/GEO-Bench-2.git@main
-pip install --user -e .[wxc,test]
+pip install --user -e .[wxc,test,geobenchv2]
 
 pytest -s --cov=terratorch -v --cov-report term-missing tests
 
--- run_pytest_container.sh
+++ run_pytest_container.sh
@@ -5,6 +5,7 @@ PYTHON_VERSIONS=("3.11" "3.12" "3.13")
 for v in ${PYTHON_VERSIONS[@]};
 do
   echo "Testing for Python $v"
-  podman build --build-arg version=$v -f Dockerfile.run
+  podman build --build-arg version=$v -f Dockerfile.run -t "pytest_terratorch_$v"
+  podman run -t "pytest_terratorch_$v" bash run_install.sh 
 done
 
