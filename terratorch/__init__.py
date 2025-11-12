7dda4290f14d99cb0c12616f98a11d83aac0cd7f
Benedikt Blumenstiel <benedikt.blumenstiel@ibm.com>
2025-11-12 17:59:19 +0100

    Mute albumentations version info
    Signed-off-by: Benedikt Blumenstiel <benedikt.blumenstiel@ibm.com>

--- terratorch/__init__.py
+++ terratorch/__init__.py
@@ -1,5 +1,9 @@
 # Copyright contributors to the Terratorch project
 
+# Mute albumentations version info
+import os
+os.environ["NO_ALBUMENTATIONS_UPDATE"] = "True"
+
 import terratorch.models  # noqa: F401
 from terratorch.models.backbones import *  # register models in registries # noqa: F403
 from terratorch.registry import BACKBONE_REGISTRY, DECODER_REGISTRY, MODEL_FACTORY_REGISTRY, FULL_MODEL_REGISTRY  # noqa: F401
