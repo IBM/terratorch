a627c69c5b0ab405f13548bac50f954717d0ae28
Benedikt Blumenstiel <benedikt.blumenstiel@ibm.com>
2025-11-17 11:41:55 +0100

    Move mulitmodal to tensor
    Signed-off-by: Benedikt Blumenstiel <benedikt.blumenstiel@ibm.com>

--- terratorch/datasets/transforms.py
+++ terratorch/datasets/transforms.py
@@ -291,6 +291,27 @@ def default_non_image_transform(array):
         return array
 
 
+class MultimodalToTensor:
+    def __init__(self, modalities):
+        self.modalities = modalities
+
+    def __call__(self, d):
+        new_dict = {}
+        for k, v in d.items():
+            if not isinstance(v, np.ndarray):
+                new_dict[k] = v
+            else:
+                if k in self.modalities and len(v.shape) >= 3:  # Assuming raster modalities with 3+ dimensions
+                    if len(v.shape) <= 4:
+                        v = np.moveaxis(v, -1, 0)  # C, H, W or C, T, H, W
+                    elif len(v.shape) == 5:
+                        v = np.moveaxis(v, -1, 1)  # B, C, T, H, W
+                    else:
+                        raise ValueError(f"Unexpected shape for {k}: {v.shape}")
+                new_dict[k] = torch.from_numpy(v)
+        return new_dict
+
+
 class MultimodalTransforms:
     """
     MultimodalTransforms applies albumentations transforms to multiple image modalities.
